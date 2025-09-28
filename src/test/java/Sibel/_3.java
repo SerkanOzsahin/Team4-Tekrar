99f6428bcf229bee87b636b6fd37a9efdc66f5de
ssibelcebeci <ssibelcebeci@gmail.com>
2025-09-28 20:52:40 +0400

    add project swag labs

--- src/test/java/Sibel/_3.java
+++ src/test/java/Sibel/_3.java
@@ -5,15 +5,10 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /***
  * bilgileri siteden alıp girin
